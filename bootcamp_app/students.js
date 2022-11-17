8121dcd39eee04ce19b162672dda156e1d434f06
patelheta <patelheta385@gmail.com>
2022-11-17 13:34:14 -0800

    Adding parameterized queries in teachers.js and students.js file

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,13 +7,15 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 let args = process.argv.slice(2);
-pool.query(`
+let values = [`%${args[0]}%`, args[1]];
+let queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${args[0]}%'
-LIMIT ${args[1] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+pool.query(queryString, values)
   .then(res => {
     console.log(res.rows);
     for (let user of res.rows) {
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,15 +6,17 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
+let args = process.argv.slice(2);
+let values = [`${args[0]}`];
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2]}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`, values)
   .then(res => {
     console.log(res.rows);
     for (let teacher of res.rows) {
